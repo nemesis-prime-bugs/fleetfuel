90535ca17568c8912954082452266eb2dac94bf2
nemesis-prime-bugs <prime-bot@outlook.com>
2026-02-08 18:42:50 +0000

    chore(ui): add tailwindcss

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  darkMode: ["class"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
